63e9f9415977fe5305d292c8889fa8b23381f75d
4lejandroquintero <alejoq96@gmail.com>
2024-02-11 20:55:12 -0500

    Arquitectura microservicio Student

--- microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
+++ microservice-course/src/main/java/com/microservice/course/controller/CourseController.java
@@ -1,32 +1,34 @@
-package com.microservice.student.controller;
+package com.microservice.course.controller;
 
-import com.microservice.student.entities.Student;
-import com.microservice.student.service.IStudentService;
+import com.microservice.course.entities.Course;
+import com.microservice.course.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/student")
-public class StudentController {
+@RequestMapping("/api/course")
+public class CourseController {
 
     @Autowired
-    private IStudentService studentService;
+    private ICourseService courseService;
 
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveStudent(@RequestBody Student student){
-        studentService.save(student);
+    public void saveStudent(@RequestBody Course course){
+        courseService.save(course);
     }
 
     @GetMapping("/all")
     public ResponseEntity<?> findAllStudent(){
-        return ResponseEntity.ok(studentService.findAll());
+
+        return ResponseEntity.ok(courseService.findAll());
     }
 
     @GetMapping("/search/{id}")
     public ResponseEntity<?> findById(@PathVariable Long id){
-        return ResponseEntity.ok(studentService.findById(id));
+
+        return ResponseEntity.ok(courseService.findById(id));
     }
 }
